d14882725601f29aacb8d4fca8abcf3c4afdab43
yongsiksim <ys4577@gmail.com>
2022-06-09 18:00:34 +0900

    read merged csv file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+import warnings
 from tqdm.notebook import tqdm
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LinearRegression, LogisticRegressionCV, RidgeCV, LassoCV, ElasticNetCV
 from sklearn import tree, svm
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
@@ -10,23 +11,27 @@ from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE, RandomOverSampler, ADASYN, BorderlineSMOTE, KMeansSMOTE, SVMSMOTE
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+warnings.filterwarnings(action='ignore')
 
-SEED = 2021 #random seed #
+SEED = 2022 #random seed #
 num_CV = 5  # number of crossvalidation #
 n_splits = 5
-repeat = 100
+repeat = 1
 test_size = 0.3 # test set ratio #
 max_iter = 100000 # max iteration for SVM #
 alphas = np.logspace(-4, 4, 17)
 
-X_ADC = pd.read_csv("results/binWidth/ADC_binWidth_32.csv")
-X_T1 = pd.read_csv("results/binWidth/T1GD_binWidth_32.csv") 
-X_T2 = pd.read_csv("results/binWidth/T2_binWidth_32.csv")
+df = pd.read_csv("data/merged.csv")
 
-X_full = X_ADC.merge(X_T1, on='ID').merge(X_T2, on='ID').iloc[:, 1:] #full set
-y = pd.read_csv("answer/answerADC.csv").iloc[:,1 ]
+training_df = df.loc[df['Outside'] == 0]
+test_df = df.loc[df['Outside'] == 1]
 
-datasets = ['X_adc_T1_T2', 'X_T1', 'X_T2', 'X_adc_T1', 'X_adc_T2', 'X_adc', 'X_T1_T2']
+X_train_full = training_df.iloc[:, 5:]
+y_train = training_df.iloc[:, 1 ]
+X_test_full = test_df.iloc[:, 5:]
+y_test = test_df.iloc[:, 1 ]
+
+datasets = ['X_adc']#['X_adc_T1_T2', 'X_T1', 'X_T2', 'X_adc_T1', 'X_adc_T2', 'X_adc', 'X_T1_T2']
 oversamples = ['original', 'ros', 'smote', 'adasyn', 'bsmote', 'svmsmote'] #'ksmote', 
 methods = ['linear', 'logistic', 'ridge', 'lasso', 'elastic', 'decisiontree', 'randomforest', 'adaboost', 'svm-linear', 'svm-poly', 'svm-rbf', 'fullyconnected']
 col_names = []
@@ -35,23 +40,27 @@ for o in oversamples:
         col_names.append("{}-{}".format(o, m))
 for d, dataset in enumerate(datasets):
     print ("[ {} / {} ]".format(d, len(datasets)), "with dataset : ", dataset, )
-    if dataset == 'X_adc_T1_T2':
-        X = X_full
-    elif dataset == 'X_T1':
-        X = X_full.iloc[:, :107]
-        X_test = X_full.iloc[:, :107]
+    if dataset == 'X_T1':
+        X_train = X_train_full.iloc[:, :107]
+        X_test = X_test_full.iloc[:, :107]
     elif dataset == 'X_T2':
-        X = X_full.iloc[:,107:214]
-        X_test = X_full.iloc[:,107:214]
+        X_train = X_train_full.iloc[:,107:214]
+        X_test = X_test_full.iloc[:,107:214]
+    elif dataset == 'X_adc_T1_T2':
+        X_train = X_train_full
+        X_test = X_test_full
     elif dataset == 'X_adc_T1':
-        X = X_full.iloc[:, :214]
+        X_train = X_train_full.iloc[:, np.r_[0:107, 214:321]]
+        X_test = X_test_full.iloc[:, np.r_[0:107, 214:321]]
     elif dataset == 'X_adc_T2':
-        X = X_full.iloc[:, np.r_[0:107, 214:321]]
+        X_train = X_train_full.iloc[:,107:321]
+        X_test = X_test_full.iloc[:,107:321]
     elif dataset == 'X_adc':
-        X = X_full.iloc[:, :107]
+        X_train = X_train_full.iloc[:, 214:]
+        X_test = X_test_full.iloc[:, 214:]
     elif dataset == 'X_T1_T2':
-        X = X_full.iloc[:,107:321]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify = y, random_state=SEED)
+        X_train = X_train_full.iloc[:, :214]
+        X_test = X_test_full.iloc[:, :214]
     df_train_result = pd.DataFrame([], columns = col_names)
     df_test_result = pd.DataFrame([], columns = col_names)
     pbar = tqdm(total=repeat*n_splits*len(oversamples)*len(methods))
@@ -68,17 +77,17 @@ for d, dataset in enumerate(datasets):
             if oversample == 'original':
                 X_sampled, y_sampled = X_train, y_train
             elif oversample == 'ros':
-                X_sampled, y_sampled = ros.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = ros.fit_resample(X_train, y_train)
             elif oversample == 'smote':
-                X_sampled, y_sampled = sm.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = sm.fit_resample(X_train, y_train)
             elif oversample == 'adasyn':
-                X_sampled, y_sampled = adas.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = adas.fit_resample(X_train, y_train)
             elif oversample == 'bsmote':
-                X_sampled, y_sampled = bsm.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = bsm.fit_resample(X_train, y_train)
             elif oversample == 'ksmote':
-                X_sampled, y_sampled = ksm.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = ksm.fit_resample(X_train, y_train)
             elif oversample == 'svmsmote':
-                X_sampled, y_sampled = ssm.fit_resample(X_train,y_train)
+                X_sampled, y_sampled = ssm.fit_resample(X_train, y_train)
             for _, method in enumerate(methods):
                 cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
                 AUC_trains = 0
@@ -89,11 +98,11 @@ for d, dataset in enumerate(datasets):
                         X_stand = sc.fit_transform(X_sampled)
                         X_stand_test = sc.transform(X_test)
                         classifier = Sequential()
-                        classifier.add(Dense(units = 128, kernel_initializer = 'uniform', activation = 'relu', input_dim = X.shape[1]))
+                        classifier.add(Dense(units = 128, kernel_initializer = 'uniform', activation = 'relu', input_dim = X_train.shape[1]))
                         classifier.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu'))
                         classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
                         classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-                        classifier.fit(X_stand[train], y_sampled.to_numpy()[train], batch_size = 10, epochs = 2)
+                        classifier.fit(X_stand[train], y_sampled.to_numpy()[train], batch_size = 10, epochs = 2, verbose = 0)
                         preds_train = classifier.predict(X_stand)
                         preds_test = classifier.predict(X_stand_test)
                     else:
@@ -131,7 +140,7 @@ for d, dataset in enumerate(datasets):
                 col_test_values.append(AUC_tests/n_splits)
         df_train_result.loc[seed] = col_train_values
         df_test_result.loc[seed] = col_test_values
-    df_train_result.to_excel("{}-{}-train.xls".format(dataset, repeat))
-    print("saved: ", "{}-{}-train.xls".format(dataset, repeat))
-    df_test_result.to_excel("{}-{}-test.xls".format(dataset, repeat))
-    print("saved: ", "{}-{}-test.xls".format(dataset, repeat))
+    df_train_result.to_excel("output/{}-{}-train.xls".format(dataset, repeat))
+    print("saved: ", "output/{}-{}-train.xls".format(dataset, repeat))
+    df_test_result.to_excel("output/{}-{}-test.xls".format(dataset, repeat))
+    print("saved: ", "output/{}-{}-test.xls".format(dataset, repeat))
